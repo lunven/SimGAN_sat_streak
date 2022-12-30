64dd35ac7135face1eed8a5061b8506c26bed63b
lunven <servanelunven@gmail.com>
2022-12-30 11:35:27 +0100

    change

---
+++ UNET/utils/prologue.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Satellite Streaks Detection')
+
+parser.add_argument('--i', type=str)
+parser.add_argument('--o', type = str)
+parser.add_argument('--k', type = str)
+parser.add_argument('--n', type = int)
+parser.add_argument('--l', type = float)
+parser.add_argument('--b', type = int)
+parser.add_argument('--v',type = int)
+
+#parser.add_argument('--learning_rate', type = float, default = 5e-4, help = 'Learning rate for the optimizer')
+args = parser.parse_args()
+
+def get_args():
+    return args
